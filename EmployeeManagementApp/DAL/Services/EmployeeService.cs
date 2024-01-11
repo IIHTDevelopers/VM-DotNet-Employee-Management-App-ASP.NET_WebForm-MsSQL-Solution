e54781338d1dfb053d93be2f5d6182b4fd79eedb
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:07:52 +0530

    Add files via upload

---
+++ EmployeeManagementApp/DAL/Services/EmployeeService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using EmployeeManagementApp.DAL.Interfaces;
+using EmployeeManagementApp.Model;
+
+namespace EmployeeManagementApp.DAL.Services
+{
+    public class EmployeeService : Interfaces.IEmployeeService
+    {
+        private Interfaces.IEmployeeRepository _repository;
+
+        public EmployeeService(Interfaces.IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public string Add()
+        {
+            return _repository.Add();
+        }
+
+        public string Update()
+        {
+            return _repository.Update();
+        }
+
+        public string Delete()
+        {
+            return _repository.Delete();
+        }
+
+        public EmployeeModel GetById(string id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
